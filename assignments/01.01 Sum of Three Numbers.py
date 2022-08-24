0c6fcd7b50d10e5cef75a1b273ec113ea77b2b8d
Christian Johnston <crjohnston89@gmail.com>
2022-08-24 15:19:48 +0000

    added first assignment

---
+++ assignments/01.01 Sum of Three Numbers.py
@@ -0,0 +1,12 @@
+x = input("Please enter a number: ")
+print("First Number: " + x)
+
+y = input("Please enter a second number: ")
+print("Second Number: " + y)
+
+z = input ("Please enter a third number: ")
+print("Third Number: " + z)
+
+s = int(x)+int(y)+int(z)
+print("The sum of the three numbers is: ")
+print(s)
