4c37de4ec535452cdcbd1426cbacbe91edfac862
Christian Johnston <crjohnston89@gmail.com>
2022-08-29 15:10:34 +0000

    commit

---
+++ assignments/unit1/01.04 Apple Sharing.py
@@ -0,0 +1,8 @@
+students = input("Please enter the number of students: ")
+apples = input("Please enter the number of apples: ")
+
+x = int(apples)/int(students)
+y = int(apples) % int(students)
+
+print(int(x))
+print(y)
