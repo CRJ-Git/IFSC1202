ce0d6517afa43a7d69f9cc17266f71adc0ea77ab
Christian Johnston <crjohnston89@gmail.com>
2022-08-29 15:23:50 +0000

    commit

---
+++ assignments/unit1/01.05 Previous and Next.py
@@ -0,0 +1,6 @@
+num = input("Please enter a number: ")
+nxt = int(num) + 1
+prev = int(num) - 1
+
+print("The next number for the number " + num + " is " + str(nxt) + ".")
+print("The previous number for the number " + num + " is " + str(prev) + ".")
