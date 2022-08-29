0a905ec349684d70495f9e47495bf0862a7be1ff
Christian Johnston <crjohnston89@gmail.com>
2022-08-29 15:32:03 +0000

    commit

--- assignments/unit1/01.06 Two Timestamps.py
+++ assignments/unit1/01.06 Two Timestamps.py
@@ -1,8 +1,8 @@
 hours1 = input("Please enter the hours for timestamp one: ")
 minutes1 = input("Please enter the minutes for timestamp one: ")
-seconds1 = input("Please enter the minutes for timestamp one: ")
+seconds1 = input("Please enter the seconds for timestamp one: ")
 
 hours2 = input("Please enter the hours for timestamp two: ")
 minutes2 = input("Please enter the minutes for timestamp two: ")
-seconds2 = input("Please enter the minutes for timestamp two: ")
+seconds2 = input("Please enter the seconds for timestamp two: ")
 
