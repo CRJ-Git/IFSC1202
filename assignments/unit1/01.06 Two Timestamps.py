29d989384592336451aeacb44025f3ce0bd5ec84
Christian Johnston <crjohnston89@gmail.com>
2022-08-29 15:38:27 +0000

    commit

--- assignments/unit1/01.06 Two Timestamps.py
+++ assignments/unit1/01.06 Two Timestamps.py
@@ -6,3 +6,7 @@ hours2 = input("Please enter the hours for timestamp two: ")
 minutes2 = input("Please enter the minutes for timestamp two: ")
 seconds2 = input("Please enter the seconds for timestamp two: ")
 
+hour_total = int(hours2) - int(hours1) * 60 * 60
+minute_total = int(minutes2) - int(minutes1) * 60
+second_total = int(seconds2) - int(seconds1)
+print(hour_total + minute_total + second_total)
