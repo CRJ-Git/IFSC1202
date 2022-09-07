1e6b8d3ea4ddf6116f0ec827558ca9b261dad699
Christian Johnston <crjohnston89@gmail.com>
2022-09-07 21:23:36 +0000

    finished unit 2 assignment 7

---
+++ assignments/unit2/02.07 Reverse Three Digits.py
@@ -0,0 +1,7 @@
+usernumber = int(input("Please enter a three digit number: "))
+
+ones = usernumber // 100
+tens = (usernumber // 10) % 10
+hundreds = usernumber % 10
+
+print("{:1d}".format(hundreds) + "{:1d}".format(tens) + "{:1d}".format(ones))
