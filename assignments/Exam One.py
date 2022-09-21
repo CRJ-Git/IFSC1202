a0b7e89135d1f5c1f6931bf92981568b9a7021c7
Christian Johnston <crjohnston89@gmail.com>
2022-09-21 14:30:39 +0000

    Exam 1

---
+++ assignments/Exam One.py
@@ -0,0 +1,30 @@
+from random import randint
+print("Winners and Losers - Human is Even, Computer is Odd")
+
+hGuess = 0
+cGuess = 0
+hScore = 0
+cScore = 0
+
+for i in range(1, 6):
+    print("Round: " + str(i))
+
+    hGuess = int(input("Enter your guess: "))
+    cGuess = randint(1,5)
+
+    print("Human Guess: " + str(hGuess) + " - " + "Computer Guess: " + str(cGuess))
+    
+    sum = hGuess + cGuess
+    
+    if sum % 2 == 0:
+        print("Sum is Even")
+        hScore += 1
+    else:
+        print("Sum is Odd")
+        cScore += 1
+    print("Human Score: " + str(hScore) + " - " + "Computer Score: " + str(cScore))
+
+if hScore > cScore:
+    print("Human Wins!")
+else:
+    print("Computer Wins!")
