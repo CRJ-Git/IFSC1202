0391f7df74e1ad942729f70d955407ce55e4de45
Christian Johnston <crjohnston89@gmail.com>
2022-10-19 20:56:17 +0000

    c

--- assignments/unit8/08.11 US Population.py
+++ assignments/unit8/08.11 US Population.py
@@ -1,4 +1,5 @@
-popFile = open("/workspace/IFSC1202/assignments/unit8/08.11 USPopulation.txt")
+popFile = open("/workspace/IFSC1202/assignments/unit8/08.11 USPopulation.txt", "r")
+
 
 x = popFile.readline()
 
@@ -6,8 +7,10 @@ popList = []
 
 while x != "":
     popList.append(int(x))
+    print(x)
+    x = popFile.readline()
 
-for i in range(0,len(popList)-1):
-    popList[i] *= 1000
+#for i in range(0,len(popList)-1):
+ #   popList[i] *= 1000
 
 print(popList[0])
