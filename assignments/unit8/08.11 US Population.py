2268a0d131910380324be901b113885afd4aa77e
Christian Johnston <crjohnston89@gmail.com>
2022-10-19 21:37:38 +0000

    c

--- assignments/unit8/08.11 US Population.py
+++ assignments/unit8/08.11 US Population.py
@@ -12,4 +12,21 @@ while x != "":
 for i in range(0,len(popList)-1):
     popList[i] *= 1000
 
-print(popList[0])
+year = 1950
+
+change = 0
+
+pChange = 0.0
+
+print("{:^6}{:^12}{:^8}{:^16}".format("Year","Population","Change","Percent Change"))
+
+print("{:^6}{:^12}{:^8}{:^16}".format(year,popList[0],change,pChange))
+
+for i in range(1,len(popList)-1):
+    year += 1
+
+    change = popList[i] - popList[i-1]
+
+    pChange = (change / popList[i-1]) * 100
+
+    print("{:^6}{:^12}{:^8}{:^16.2}".format(year,popList[i],change,pChange))
