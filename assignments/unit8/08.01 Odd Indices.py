060765fb10f9092f5b9386c79428f43dfd5f13e5
Christian Johnston <crjohnston89@gmail.com>
2022-10-19 16:37:41 +0000

    c

--- assignments/unit8/08.01 Odd Indices.py
+++ assignments/unit8/08.01 Odd Indices.py
@@ -1,1 +1,6 @@
-x = input("Please enter a string containing integers separated by spaces.")
+x = input("Please enter a string containing integers separated by spaces: ")
+
+y = x.split(' ')
+
+for i in range(1, len(y), 2):
+    print(y[i])
