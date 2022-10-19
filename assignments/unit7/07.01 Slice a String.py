241bd5439718fa6ae08acf6bd74810345f700b2d
Christian Johnston <crjohnston89@gmail.com>
2022-10-19 14:26:35 +0000

    commit

--- assignments/unit7/07.01 Slice a String.py
+++ assignments/unit7/07.01 Slice a String.py
@@ -1,5 +1,17 @@
-x = input("Please input a string of characters.")
+x = str("1234567890")#input("Please input a string of characters.")
 
 print(x[2])
 
 print (x[-2])
+
+print(x[0:5])
+
+print(x[:-2])
+
+print(x[0::2])
+
+print(x[1::2])
+
+print(x[-1:0:-1])
+
+
