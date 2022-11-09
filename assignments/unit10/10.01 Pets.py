28f51bcc43080a09aa301551ac12ab9ee478e257
Christian Johnston <crjohnston89@gmail.com>
2022-11-09 17:26:20 +0000

    commit

--- assignments/unit10/10.01 Pets.py
+++ assignments/unit10/10.01 Pets.py
@@ -3,3 +3,21 @@ class Pet ():
         self.name = name
         self.type = type
         self.age = age
+
+petfile = open("/workspace/IFSC1202/assignments/unit10/10.01 Pets.txt")
+
+line = petfile.readline()
+
+petArray = []
+
+while line != "":
+    #v for vector
+    v = line.split(", ")
+    newPet = Pet(v[0], v[1], v[2])
+    petArray.append(newPet)
+    line = petfile.readline()
+
+print("{:^10}{:^10}{:^10}" .format("Name", "Type", "Age"))
+
+for i in range(len(petArray)):
+    print("{:^10}{:^10}{:^10}" .format(petArray[i].name, petArray[i].type, petArray[i].age))
