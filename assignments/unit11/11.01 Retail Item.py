98585fc18278786d5a926bc9d30b083cc1b8871a
Christian Johnston <crjohnston89@gmail.com>
2022-11-14 16:07:23 +0000

    c

--- assignments/unit11/11.01 Retail Item.py
+++ assignments/unit11/11.01 Retail Item.py
@@ -1,5 +1,5 @@
 class RetailItem():
-    def __init__(self, description = "", unitsonhand = 0, price = 0):
+    def __init__(self, description = "", unitsonhand = 0, price = 0.0):
         self.description = description
         self.unitsonhand = unitsonhand
         self.price = price
