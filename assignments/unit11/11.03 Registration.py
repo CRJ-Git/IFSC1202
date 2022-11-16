d7b2136948d53cac685770c1b67b5441c09f7052
Christian Johnston <crjohnston89@gmail.com>
2022-11-16 22:27:12 +0000

    c

--- assignments/unit11/11.03 Registration.py
+++ assignments/unit11/11.03 Registration.py
@@ -3,7 +3,7 @@ class Student():
         self.firstname = firstname
         self.lastname = lastname
         self.tnumber = tnumber
-        courselist = []
+        courseList = []
 
 class StudentList():
     def __init__(self):
@@ -26,5 +26,67 @@ class StudentList():
             for j in range(len(studentvector[i].courselist)):
                 print("{:<15}{:<50}{:<14}{:<15}".format(studentvector[i].courselist[j].department + " " + studentvector[i].courselist[j].number, studentvector[i].courselist[j].name, studentvector[i].courselist[j].room, studentvector[i].courselist[j].meetingtimes))
     
+    
     def add_student_from_file(self, filename):
-        fin = open("/workspace/IFSC1202/assignments/unit11/11.03 Students.txt")
+        #filename = "/workspace/IFSC1202/assignments/unit11/11.03 Students.txt"
+        fin = open(filename)
+        line = fin.readline()
+        while line != "":
+            v = line.split(",")
+            v[2] = v[2].replace("\n", "")
+            add_student(v[0], v[1], v[2])
+            line = fin.readline()
+
+class Course():
+    def __init__(self, department, number, name, room, meetingtimes):
+        self.department = department
+        self.number = number
+        self.name = name
+        self.room = room
+        self.meetingtimes = meetingtimes
+
+class CourseList():
+    def __init__(self):
+        courselist = []
+    
+    def add_course(self, department, number, name, room, meetingtimes):
+        newCourse = Course(department, number, name, room, meetingtimes)
+        courselist.append(newCourse)
+    
+    def find_course(self, departmenttofind, numbertofind):
+        for i in range(len(courselist)):
+            if courselist[i].department == departmenttofind and courselist[i].number == numbertofind:
+                return i
+        return -1
+    
+    def add_course_from_file(self, filename):
+        #filename ="/workspace/IFSC1202/assignments/unit11/11.03 Courses.txt"
+        fin = open(filename)
+        line = fin.readline()
+        
+        while line != "":
+            v = line.split(",")
+            v[4] = v[4].replace("\n", "")
+            CourseList.add_course(v[0], v[1], v[2], v[3], v[4])
+
+#start of main program:
+
+mainCourseList = CourseList()
+
+mainCourseList.add_course_from_file("/workspace/IFSC1202/assignments/unit11/11.03 Courses.txt")
+
+mainStudentList = StudentList()
+
+mainStudentList.add_student_from_file("/workspace/IFSC1202/assignments/unit11/11.03 Students.txt")
+
+fin = open("/workspace/IFSC1202/assignments/unit11/11.03 Registration.txt")
+line = fin.readline()
+
+while line != "":
+    v = line.split(",")
+    v[2] = v[2].replace("\n", "")
+    coursetoadd = mainCourseList.courselist[mainCourseList.find_course(v[1], v[2])]
+    mainStudentList.studentvector[mainStudentList.find_student(v[0])].CourseList.append(coursetoadd)
+    line = fin.readline()
+
+mainStudentList.print_student_list()
